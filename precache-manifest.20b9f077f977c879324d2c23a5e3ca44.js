d860455e2f77c0c970e3c87047ce6c23724cbf1a
Hariprasath Lakshmanan <iamharry-dev>
2022-04-08 17:05:22 +0530

    Updates

---
+++ precache-manifest.20b9f077f977c879324d2c23a5e3ca44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307240f926df3a3aec85edc9f2b74fa3",
+    "url": "/Yolo_farm_fresh/index.html"
+  },
+  {
+    "revision": "8ed663ccf812536c6a47",
+    "url": "/Yolo_farm_fresh/static/css/2.daf4b330.chunk.css"
+  },
+  {
+    "revision": "630f4772f82dfb48b0e0",
+    "url": "/Yolo_farm_fresh/static/css/main.da51e205.chunk.css"
+  },
+  {
+    "revision": "8ed663ccf812536c6a47",
+    "url": "/Yolo_farm_fresh/static/js/2.18c0e3af.chunk.js"
+  },
+  {
+    "revision": "205ca6362c94db6602fc89aa561e75e5",
+    "url": "/Yolo_farm_fresh/static/js/2.18c0e3af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630f4772f82dfb48b0e0",
+    "url": "/Yolo_farm_fresh/static/js/main.9781d0e6.chunk.js"
+  },
+  {
+    "revision": "2deba7650d06301f833e",
+    "url": "/Yolo_farm_fresh/static/js/runtime-main.78da8ca6.js"
+  }
+]);
